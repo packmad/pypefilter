2800c42821731e4e3cce665d9c1d847aa589ecab
packmad <simone.aonzo@gmail.com>
2020-12-02 23:56:24 +0100

    auto

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypefilter",
-    version="0.0.1",
+    version="0.0.3",
     author="Simone Aonzo",
     author_email="simone.aonzo@gmail.com",
     description="PyPEfilter filters out non-native Portable Executable",
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/packmad/pypefilter",
     packages=setuptools.find_packages(),
+    entry_points = {'console_scripts': ['pypefilter = pypefilter.pypefilter:main']},
+    install_requires=['python-magic'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
