3ccfb03269ddac04ae1d51d7a6dcedc642757721
packmad <simone.aonzo@gmail.com>
2020-12-16 16:42:46 +0100

    auto

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypefilter",
-    version="0.0.3",
+    version="0.0.4",
     author="Simone Aonzo",
     author_email="simone.aonzo@gmail.com",
     description="PyPEfilter filters out non-native Portable Executable",
