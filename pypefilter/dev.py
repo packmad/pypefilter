21018bbe93ce4b3f003a8ced6750b7c73b725a0b
packmad <simone.aonzo@gmail.com>
2021-08-23 21:50:40 +0200

    tqdm

--- pypefilter/dev.py
+++ pypefilter/dev.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 
 start_folder = "C:\\"; assert isdir(start_folder)
-dst_folder = 'S:\\'; assert isdir(dst_folder)
+dst_folder = 'S:\\goodware2021'; assert isdir(dst_folder)
 exe_files_json = 'exe_files.json'
 
 
