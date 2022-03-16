80f68a580fd500c2afda93b2f0c97c3499f9e7e6
Simone Aonzo <simone.aonzo@gmail.com>
2022-03-16 18:52:23 +0100

    refactoring

--- pypefilter/pypefilter.py
+++ pypefilter/pypefilter.py
@@ -2,19 +2,25 @@
 import argparse
 import hashlib
 import os
+from typing import Optional
+from collections import Counter
+
 import magic  # pip3 install python-magic
 import shutil
 import sys
 
 from multiprocessing import Pool, freeze_support
-from itertools import repeat
 from os.path import isdir, isfile, join
 from pathlib import Path
+from tqdm import tqdm  # pip3 install tqdm
 
 
+DELETE: bool = False
+RENAME: bool = False
+VERBOSE: bool = False
+VERBOSE_MAGIC: bool = False
+DST_FOLDER: Optional[str] = None
 BLACKLIST = ['DLL', '.Net', 'Installer', 'ARM']
-VERBOSE = False
-VERBOSE_MAGIC = False
 
 
 def vprint(action: str, fmagic: str, src: str, dst: str):
@@ -39,59 +45,59 @@ def is_pe(file_path: str) -> bool:
         return False
 
 
-def pe_filter(pe_magic: str) -> bool:
-    return pe_magic.startswith('PE32') and not any(x in pe_magic for x in BLACKLIST)
-
-
-def check(file_path: str, dst_folder: str, remove_not_matching: bool, rename: bool):
+def check(file_path: str):
     file_magic = None
     is_native_pe = False
     if is_pe(file_path):
         file_magic = magic.from_file(file_path)
-        if pe_filter(file_magic):
+        if file_magic.startswith('PE32') and not any(x in file_magic for x in BLACKLIST):
             is_native_pe = True
-            if rename or dst_folder is not None:
+            if RENAME or DST_FOLDER is not None:
                 file_sha256sum = get_file_sha256sum(file_path)
-                if dst_folder is not None:
+                if DST_FOLDER is not None:
                     try:
-                        dst_file_sha256 = join(dst_folder, f'{file_sha256sum}')
+                        dst_file_sha256 = join(DST_FOLDER, f'{file_sha256sum}')
                         if not isfile(dst_file_sha256):
                             shutil.copyfile(file_path, dst_file_sha256)
                             vprint('C', file_magic, file_path, dst_file_sha256)
                     except Exception as e:
                         print(f'[!][{file_magic}] {file_path}', file=sys.stderr)
                         print(e, file=sys.stderr)
-                if rename:
+                if RENAME:
                     dst_path = join(Path(file_path).parent, file_sha256sum)
                     if file_path != dst_path:
                         vprint('R', file_magic, file_path, dst_path)
                         os.rename(file_path, dst_path)
-    if remove_not_matching and not is_native_pe:
+    if DELETE and not is_native_pe:
         vprint('X', file_magic, file_path, '/dev/null')
         os.remove(file_path)
+    return file_magic
 
 
-def parallel_filter(start_folder: str, dst_folder: str, delete: bool, rename: bool):
-    with Pool() as pool:
+def parallel_filter(start_folder: str, dst_folder: str):
+    with Pool(processes=4) as pool:
         for root, dirs, files in os.walk(start_folder, topdown=False):
             print('>>>', root)
-            pool.starmap(check,
-                         zip([join(root, name) for name in files], repeat(dst_folder), repeat(delete), repeat(rename)))
+            r = list(tqdm(pool.imap(check, [join(root, f) for f in files]), total=len(files)))
+            print(Counter(r))
             print('<<<', root)
 
 
-def main():
-    global BLACKLIST, VERBOSE, VERBOSE_MAGIC
+if __name__ == '__main__':
+    global BLACKLIST, VERBOSE, VERBOSE_MAGIC, DELETE, RENAME, DST_FOLDER
     freeze_support()
-    parser = argparse.ArgumentParser(description='PyPEfilter filters out non-native Portable Executable files')
+    parser = argparse.ArgumentParser(
+        description='PyPEfilter filters out non-native Portable Executable files')
     parser.add_argument('-s', '--src', type=str, help='Source directory', required=True)
     parser.add_argument('-d', '--dst', type=str, help='Destination directory')
-    parser.add_argument('--rename', help='Rename matching files with their sha256 hash', action='store_true')
+    parser.add_argument('--rename', help='Rename matching files with their sha256 hash',
+                        action='store_true')
     parser.add_argument('--delete', help='Delete non-matching files', action='store_true')
     parser.add_argument('--no64', help='Exclude PE for the 64bit arch', action='store_true')
     me_group = parser.add_mutually_exclusive_group()
     me_group.add_argument('-v', '--verbose', help='Display messages', action='store_true')
-    me_group.add_argument('-vm', '--vmagic', help='Display messages and magic of non-matching files',
+    me_group.add_argument('-vm', '--vmagic',
+                          help='Display messages and magic of non-matching files',
                           action='store_true')
     args = parser.parse_args()
     assert isdir(args.src)
@@ -103,8 +109,7 @@ def main():
         BLACKLIST.append('x86-64')
     VERBOSE = args.verbose or args.vmagic
     VERBOSE_MAGIC = args.vmagic
-    parallel_filter(args.src, args.dst, args.delete, args.rename)
-
-
-if __name__ == '__main__':
-    main()
+    DELETE = args.delete
+    RENAME = args.rename
+    DST_FOLDER = args.dst
+    parallel_filter(args.src)
